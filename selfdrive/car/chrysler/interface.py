c578d55f70227006dcb197723c1f8228476664a0
Andrew Christiansen <andrrocity@gmail.com>
2020-05-18 01:16:29 -0700

    Playing with Chrysler 300 lateral PID and other values trying to minimize ping pong.

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -37,10 +37,12 @@ class CarInterface(CarInterfaceBase):
       ret.steerActuatorDelay = 0.2  # in seconds
 
     if candidate in (CAR.CHRYSLER_300_2018):
-        ret.wheelbase = 3.05308 # in meters
-        ret.steerRatio = 16.1
-        ret.mass = 1859.729 + STD_CARGO_KG
-        
+      ret.wheelbase = 3.05308 # in meters
+      ret.steerRatio = 15.5 # 2013 V-6 (RWD) — 15.5:1 V-6 (AWD) — 16.5:1 V-8 (RWD) — 15.5:1 V-8 (AWD) — 16.5:1
+      ret.mass = 1828.0 + STD_CARGO_KG # 2013 V-6 RWD
+      ret.lateralTuning.pid.kf = 0.00005584521385
+      ret.steerRateCost = 0.6
+
     ret.centerToFront = ret.wheelbase * 0.44
 
     ret.minSteerSpeed = 3.8  # m/s
