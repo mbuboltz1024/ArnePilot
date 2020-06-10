544b63a811f3e4fc9a0afe5b96817c2605f09b99
Andrew Christiansen <andrrocity@gmail.com>
2020-06-10 00:03:41 -0500

    testing

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -40,9 +40,9 @@ class CarInterface(CarInterfaceBase):
       ret.wheelbase = 3.05308 # in meters
       ret.steerRatio = 15.5 # 2013 V-6 (RWD) — 15.5:1 V-6 (AWD) — 16.5:1 V-8 (RWD) — 15.5:1 V-8 (AWD) — 16.5:1
       ret.mass = 1828.0 + STD_CARGO_KG # 2013 V-6 RWD
-      ret.lateralTuning.pid.kf = 0.00006   # full torque for 10 deg at 80mph means 0.00007818594
-      ret.steerLimitTimer = 0.1
-      ret.steerRateCost = 0.7
+      # ret.lateralTuning.pid.kf = 0.00006   # full torque for 10 deg at 80mph means 0.00007818594
+      # ret.steerLimitTimer = 0.1
+      ret.steerRateCost = 0.3
 
     ret.centerToFront = ret.wheelbase * 0.44
 
